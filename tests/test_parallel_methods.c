012d74a160645a9eaf20f4c8732d4ad1965b9b66
Migunova Anastasia <anastasiamigunova76@gmail.com>
2026-05-19 03:21:30 +0300

    ci: init (forgot to add)

--- tests/test_parallel_methods.c
+++ tests/test_parallel_methods.c
@@ -9,7 +9,7 @@
 #include "core_builder.h"
 #include "proc_image_utils.h"
 
-#define IMAGE_DIR "./images"
+#define IMAGE_DIR "./../tests/data/300x120.jpg"
 
 size_t task_granularity_k = 3;
 size_t grid_granularity_k = 1;
@@ -38,7 +38,7 @@ static int setup(void **state)
 	assert_non_null(ctx);
 	char input_file[512];
 	char *file_name = get_default_input(IMAGE_DIR);
-	snprintf(input_file, 512, "%s/%s",IMAGE_DIR, file_name);
+	snprintf(input_file, 512, "%s/%s", IMAGE_DIR, file_name);
 	free(file_name);
 
 	ctx->input_image = load_image(input_file,
